b5a060978a8a8f3dc2684b63bba23be0f28769ac
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-12-11 15:48:18 -0300

    API movida a otro repositorio

--- rest_api/api/asistencia/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ListarProfesores, ListarAlumnos
-
-urlpatterns = [
-    path('profesoresList', ListarProfesores.as_view(), name='profesores-list'),
-    path('alumnosList', ListarAlumnos.as_view(), name='alumnos-list'),
-]
